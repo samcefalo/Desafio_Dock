34b718eca8d156f579748f2e4da2c60a27417973
Samcefalo <samuel.cefalo@gmail.com>
2021-09-30 13:45:29 -0300

    Lidando e tratando Exceptions

--- src/main/java/me/samcefalo/desafio/repositories/exceptions/EntidadeExceptionHandler.java
+++ src/main/java/me/samcefalo/desafio/repositories/exceptions/EntidadeExceptionHandler.java
@@ -3,6 +3,8 @@ package me.samcefalo.desafio.repositories.exceptions;
 import me.samcefalo.desafio.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,5 +23,18 @@ public class EntidadeExceptionHandler {
         StandardError standardError = new StandardError(HttpStatus.NOT_FOUND.value(), error.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(standardError);
     }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<StandardError> typeNotSupported(HttpMediaTypeNotSupportedException error, HttpServletRequest request) {
+        StandardError standardError = new StandardError(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(), error.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()).body(standardError);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<StandardError> typeNotSupported(HttpRequestMethodNotSupportedException error, HttpServletRequest request) {
+        StandardError standardError = new StandardError(HttpStatus.METHOD_NOT_ALLOWED.value(), error.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED.value()).body(standardError);
+    }
+
 
 }
