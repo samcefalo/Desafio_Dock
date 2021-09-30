3d56b62811cb067db6c1d388af410d707e396427
Samcefalo <samuel.cefalo@gmail.com>
2021-09-30 03:41:54 -0300

    Criando NotFoundException

---
+++ src/main/java/me/samcefalo/desafio/repositories/exceptions/EntidadeExceptionHandler.java
@@ -0,0 +1,27 @@
+package me.samcefalo.desafio.repositories.exceptions;
+
+import me.samcefalo.desafio.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+/*
+ * @ControllerAdvice - lida com Exceptions da aplicação
+ * Interceptor de exceções lançadas por métodos
+ */
+
+@ControllerAdvice
+public class EntidadeExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException error, HttpServletRequest request) {
+        StandardError standardError = new StandardError(HttpStatus.NOT_FOUND.value(), error.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(standardError);
+    }
+
+}
---
+++ src/main/java/me/samcefalo/desafio/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package me.samcefalo.desafio.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
