6779ce8592ac9021e4991f5336e519343fac5128
Samcefalo <samuel.cefalo@gmail.com>
2021-09-30 02:14:00 -0300

    Primeiro commit - iniciando os profiles

---
+++ src/main/java/me/samcefalo/desafio/DesafioApplication.java
@@ -0,0 +1,13 @@
+package me.samcefalo.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/samcefalo/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.samcefalo.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
