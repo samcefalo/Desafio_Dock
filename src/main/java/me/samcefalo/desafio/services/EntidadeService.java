879d0c07ab788f71695eadb31683491214901c64
Samcefalo <samuel.cefalo@gmail.com>
2021-09-30 03:44:28 -0300

    Iniciando find na camada de serviço

--- src/main/java/me/samcefalo/desafio/services/EntidadeService.java
+++ src/main/java/me/samcefalo/desafio/services/EntidadeService.java
@@ -1,6 +1,8 @@
 package me.samcefalo.desafio.services;
 
+import me.samcefalo.desafio.domain.Entidade;
 import me.samcefalo.desafio.repositories.EntidadeRepository;
+import me.samcefalo.desafio.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,10 @@ public class EntidadeService {
     private EntidadeRepository entidadeRepository;
 
     //TODO fazer regras de negocios
+
+    public Entidade find(int logic) {
+        return entidadeRepository.findByLogic(logic).orElseThrow(
+                () -> new ObjectNotFoundException("Objeto não encontrado. Logic: " + logic + ", Tipo: " + Entidade.class.getName()));
+    }
 
 }
