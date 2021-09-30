863bf05913bfb49bf7bebd50e1b5c13905ba7351
Samcefalo <samuel.cefalo@gmail.com>
2021-09-30 02:36:03 -0300

    Criando objeto Entidade

---
+++ src/main/java/me/samcefalo/desafio/domain/Entidade.java
@@ -0,0 +1,33 @@
+package me.samcefalo.desafio.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@Entity
+public class Entidade implements Serializable {
+
+    /*
+     * TODO criar DTO com validações
+     * "required": ["logic", "serial", "model", "version"]
+     */
+
+    @Id
+    private int logic;
+    private String serial;
+    private String model;
+    // Validação min = 0 -> NotEmpty
+    private int sam;
+    private String ptid;
+    private int plat;
+    private String version;
+    private int mxr;
+    private String mxf;
+    private String verfm;
+
+}
