e76465843f3083a5354ef170ffa5f6dd9c0b2874
Samcefalo <samuel.cefalo@gmail.com>
2021-09-30 02:45:29 -0300

    Criando camada de controladores REST da Entidade

--- src/main/java/me/samcefalo/desafio/resources/EntidadeResource.java
+++ src/main/java/me/samcefalo/desafio/resources/EntidadeResource.java
@@ -6,13 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * Controladores - Controla o fluxo da aplicação (Requests)
+ */
+
 @RestController
 @RequestMapping
 public class EntidadeResource {
 
+    // Camada de serviço
     @Autowired
     private EntidadeService entidadeService;
-    
+
     /*
      * TODO Desmembrar a entrada, armazenar em uma entidade e retornar um json
      *
