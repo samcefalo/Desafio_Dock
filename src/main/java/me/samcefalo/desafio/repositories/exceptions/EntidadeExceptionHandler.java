40b8587b2f82ae7ec585a594805a057b5754cd19
Samcefalo <samuel.cefalo@gmail.com>
2021-09-30 03:43:02 -0300

    Criando NotFoundException

--- src/main/java/me/samcefalo/desafio/repositories/exceptions/EntidadeExceptionHandler.java
+++ src/main/java/me/samcefalo/desafio/repositories/exceptions/EntidadeExceptionHandler.java
@@ -3,8 +3,6 @@ package me.samcefalo.desafio.repositories.exceptions;
 import me.samcefalo.desafio.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
