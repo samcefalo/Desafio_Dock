e48535d08b87114c7e4f0c53dfc01a0ad19c8802
Samcefalo <samuel.cefalo@gmail.com>
2021-10-01 10:31:22 -0300

    Correção do construtor JsonValidationFailedException

--- src/main/java/me/samcefalo/desafio/services/exceptions/JsonValidationFailedException.java
+++ src/main/java/me/samcefalo/desafio/services/exceptions/JsonValidationFailedException.java
@@ -3,16 +3,17 @@ package me.samcefalo.desafio.services.exceptions;
 import com.networknt.schema.ValidationMessage;
 import lombok.Getter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class JsonValidationFailedException extends RuntimeException {
 
     @Getter
-    Set<ValidationMessage> validationResult;
+    Set<ValidationMessage> validationResult = new HashSet<>();
 
     public JsonValidationFailedException(String message, Set<ValidationMessage> validationResult) {
         super(message);
-        this.validationResult = validationResult;
+        this.validationResult.addAll(validationResult);
     }
 
     public JsonValidationFailedException(String message, Throwable cause) {
