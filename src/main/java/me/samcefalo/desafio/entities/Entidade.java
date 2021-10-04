412408248aa88cd2a22ed2ba15a50b81d0a2c00d
Samcefalo <samuel.cefalo@gmail.com>
2021-10-04 10:19:52 -0300

    Colunas da Entidade mapeadas

--- src/main/java/me/samcefalo/desafio/entities/Entidade.java
+++ src/main/java/me/samcefalo/desafio/entities/Entidade.java
@@ -2,6 +2,7 @@ package me.samcefalo.desafio.entities;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.Serializable;
@@ -11,15 +12,25 @@ import java.io.Serializable;
 public class Entidade implements Serializable {
 
     @Id
+    @Column(name = "logic")
     private int logic;
+    @Column(name = "serial", length = 32)
     private String serial;
+    @Column(name = "model", length = 32)
     private String model;
+    @Column(name = "sam")
     private int sam;
+    @Column(name = "ptid", length = 32)
     private String ptid;
+    @Column(name = "plat")
     private int plat;
+    @Column(name = "version", length = 32)
     private String version;
+    @Column(name = "mxr")
     private int mxr;
+    @Column(name = "mxf")
     private int mxf;
+    @Column(name = "verfm", length = 32)
     private String verfm;
 
     public Entidade() {
