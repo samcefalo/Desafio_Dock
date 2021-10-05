478e691c02a207b3798acba54335f07ef3f10fea
Samcefalo <samuel.cefalo@gmail.com>
2021-10-04 22:02:27 -0300

    Abstração do método parseFromString

--- src/main/java/me/samcefalo/desafio/controllers/EntidadeController.java
+++ src/main/java/me/samcefalo/desafio/controllers/EntidadeController.java
@@ -28,7 +28,7 @@ public class EntidadeController {
     @RequestMapping(method = RequestMethod.POST, produces = "application/json", consumes = "text/html")
     public ResponseEntity<Entidade> insert(@RequestBody String string) {
         //formato: 44332211;123;PWWIN;0;F04A2E4088B;4;8.00b3;0;16777216;PWWIN
-        Entidade entidade = ObjectParser.parseFromString(string, ";");
+        Entidade entidade = ObjectParser.parseFromString(string, Entidade.class, ";");
         validation.validateJson(entidade);
         return ResponseEntity.status(HttpStatus.CREATED).body(entidadeService.insert(entidade));
     }
--- src/main/java/me/samcefalo/desafio/resources/utils/ObjectParser.java
+++ src/main/java/me/samcefalo/desafio/resources/utils/ObjectParser.java
@@ -1,29 +1,52 @@
 package me.samcefalo.desafio.resources.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import me.samcefalo.desafio.entities.Entidade;
+import me.samcefalo.desafio.services.exceptions.JsonValidationFailedException;
 import me.samcefalo.desafio.services.exceptions.ObjectOutOfBoundsException;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ObjectParser {
 
-    public static Entidade parseFromString(String string, String separator) {
-        String[] vetor = string.split(separator);
-        Entidade entidade = new Entidade();
-        //Entidade
+    public static Entidade parseFromString(String string, Class<?> tClass, String separator) {
         try {
-            entidade.setLogic(Integer.parseInt(vetor[0]));
-            entidade.setSerial(vetor[1]);
-            entidade.setModel(vetor[2]);
-            entidade.setSam(Integer.parseInt(vetor[3]));
-            entidade.setPtid(vetor[4]);
-            entidade.setPlat(Integer.parseInt(vetor[5]));
-            entidade.setVersion(vetor[6]);
-            entidade.setMxr(Integer.parseInt(vetor[7]));
-            entidade.setMxf(Integer.parseInt(vetor[8]));
-            entidade.setVerfm(vetor[9]);
+            return deserialize(toJson(string, tClass, separator));
+        } catch (JsonProcessingException e) {
+            throw new JsonValidationFailedException("Erro de validação.", e);
+        }
+    }
+
+    private static Entidade deserialize(String string) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(string, Entidade.class);
+    }
+
+    private static String toJson(String string, Class<?> tClass, String separator) {
+        String[] vetor = string.split(separator);
+        Map<String, String> jsonElements = new HashMap<>();
+        try {
+            for (int i = 0; i < tClass.getDeclaredFields().length; i++) {
+                Field field = tClass.getDeclaredFields()[i];
+                field.setAccessible(true);
+                jsonElements.put(field.getName(), vetor[i]);
+            }
         } catch (ArrayIndexOutOfBoundsException e) {
             throw new ObjectOutOfBoundsException("Erro de inserção: Variáveis insuficientes ou vazia.");
         }
-        return entidade;
+        return toJsonString(jsonElements);
+    }
+
+    private static String toJsonString(Map<String, String> jsonMap) {
+        String elementsString = jsonMap.entrySet()
+                .stream()
+                .map(entry -> "\"" + entry.getKey() + "\":\"" + entry.getValue() + "\"")
+                .collect(Collectors.joining(","));
+        return "{" + elementsString + "}";
     }
 
 }
